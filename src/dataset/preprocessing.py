074c5952128afde79252a9ebf979b429513db9f1
S.Amin Hossaini <s.aminhsn@SAmins-MacBook-Air.local>
2023-06-26 07:57:11 +0330

    preprocess

--- src/dataset/preprocessing.py
+++ src/dataset/preprocessing.py
@@ -1,8 +1,62 @@
-import spacy  # I'll be making use of spacy for text preprocessing
-from spacy.lang.en.stop_words import STOP_WORDS
-from spacy.lang.en import English
-import string, re
+from hazm import *
+import os
 
 
 def preprocessor():
-    ...
+    
+    input = "/Users/aminh/Desktop/IR-project/dataset/"
+    output = "/Users/aminh/Desktop/IR-project/Information_Retrieval_Project/NormalizeDataset/"
+    normalizer = Normalizer()
+
+    for subDir in os.listdir(input):
+        #create root
+        if not os.path.exists(output):
+            os.makedirs(output)
+        
+        #create directories in root
+        if not os.path.exists(os.path.join(output, f'{subDir}')):
+            os.makedirs(os.path.join(output, f'{subDir}'))
+        
+        #normalize files of each directory
+        for root, dirs, files in os.walk(os.path.join(input, subDir)):
+            for file in files:
+                if file.endswith(".txt"):
+                    path = os.path.join(root, file)
+                    with open(path, 'r') as f:
+                        text = f.read()
+                        normText = normalizer.normalize(text)
+                    
+                    path = os.path.join(output, subDir, file)
+                    with open(path, 'w') as f:
+                        f.write(normText)
+
+
+def tokenizer():
+    input = "/Users/aminh/Desktop/IR-project/Information_Retrieval_Project/NormalizeDataset/"
+    output = "/Users/aminh/Desktop/IR-project/Information_Retrieval_Project/TokenizeDataset/"
+
+    for subDir in os.listdir(input):
+        #create root
+        if not os.path.exists(output):
+            os.makedirs(output)
+        
+        #create directories in root
+        if not os.path.exists(os.path.join(output, f'{subDir}')):
+            os.makedirs(os.path.join(output, f'{subDir}'))
+        
+        #tokenize files of each directory
+        for root, dirs, files in os.walk(os.path.join(input, subDir)):
+            for file in files:
+                if file.endswith(".txt"):
+                    path = os.path.join(root, file)
+                    with open(path, 'r') as f:
+                        text = f.read()
+                        tokenText = word_tokenize(text)
+                    
+                    path = os.path.join(output, subDir, file)
+                    with open(path, 'w') as f:
+                        f.write(str(tokenText))
+
+
+preprocessor()
+tokenizer()
